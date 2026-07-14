91dbeb734494222067bf0956ccdbec996f3faace
Official Bika <phoetaay@gmail.com>
2026-07-14 19:45:07 +0700

    Add reusable dynamic brand text component

---
+++ frontend/components/BrandText.tsx
@@ -0,0 +1,9 @@
+import { nftDisplayName, coinDisplayName } from '@/lib/names';
+
+export function NFTName(){
+ return <>{nftDisplayName}</>;
+}
+
+export function CoinName(){
+ return <>{coinDisplayName}</>;
+}
