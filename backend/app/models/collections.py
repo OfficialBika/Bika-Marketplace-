ab9291117c824891bea460e644841a5d6e54472e
Official Bika <phoetaay@gmail.com>
2026-07-14 20:00:15 +0700

    Add MongoDB marketplace collection definitions

---
+++ backend/app/models/collections.py
@@ -0,0 +1,13 @@
+"""MongoDB collection names for Bika Marketplace.
+
+Existing bika_bot collections are not modified.
+"""
+
+MARKETPLACE_LISTINGS = 'marketplace_listings'
+MARKETPLACE_ORDERS = 'marketplace_orders'
+AUCTIONS = 'auctions'
+BIDS = 'bids'
+TRADES = 'trades'
+WALLET_TRANSACTIONS = 'wallet_transactions'
+SPIN_HISTORY = 'spin_history'
+AUDIT_LOGS = 'audit_logs'
