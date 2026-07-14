ff6b5292c923dece2c25de3ecff0bc3a997be7c6
Official Bika <phoetaay@gmail.com>
2026-07-14 20:34:51 +0700

    Add production configuration settings

---
+++ backend/app/config/settings.py
@@ -0,0 +1,11 @@
+import os
+
+class Settings:
+    APP_NAME = os.getenv('APP_NAME', 'Bika Marketplace')
+    DB_NAME = os.getenv('DB_NAME', 'bika_bot')
+    COIN_NAME = os.getenv('COIN_NAME', 'BKC')
+    NFT_NAME = os.getenv('NFT_NAME', 'BIKA')
+    MONGO_URL = os.getenv('MONGO_URL', 'mongodb://localhost:27017')
+    ENV = os.getenv('ENV', 'development')
+
+settings = Settings()
