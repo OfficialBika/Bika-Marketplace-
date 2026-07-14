8a305793d1921f1e3b4910bc386f8ee7b5bc4d3a
Official Bika <phoetaay@gmail.com>
2026-07-14 20:50:31 +0700

    Refactor character service to use repository layer

--- backend/app/services/character_service.py
+++ backend/app/services/character_service.py
@@ -1,7 +1,25 @@
-from app.database.mongodb import db
+from app.repositories.character_repository import character_repository
+
+
+class CharacterService:
+    async def get_character(self, character_id: str):
+        character = await character_repository.get_by_id(character_id)
+
+        if character:
+            character['_id'] = str(character['_id'])
+
+        return character
+
+    async def get_user_characters(self, user_id: str):
+        return await character_repository.list_by_owner(user_id)
+
+
+character_service = CharacterService()
+
 
 async def get_character(character_id: str):
-    return await db.characters.find_one({"_id": character_id})
+    return await character_service.get_character(character_id)
+
 
 async def get_user_characters(user_id: str):
-    return await db.characters.find({"owner_id": user_id}).to_list(100)
+    return await character_service.get_user_characters(user_id)
