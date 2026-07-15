d9286d7fb2d4191deb130852e6dd5df3d31aa68f
Official Bika <phoetaay@gmail.com>
2026-07-15 14:12:14 +0700

    Update frontend env utility from Bika Marketplace v28 ZIP

--- frontend/lib/env.ts
+++ frontend/lib/env.ts
@@ -1,4 +1,6 @@
 export const env = {
  API_URL: process.env.NEXT_PUBLIC_API_URL || "",
- RARITY: process.env.NEXT_PUBLIC_SHOW_RARITY || "Supreme"
+ RARITY: process.env.NEXT_PUBLIC_SHOW_RARITY || "Supreme",
+ NFT_NAME: process.env.NEXT_PUBLIC_NFT_NAME || "BIKA",
+ COIN_NAME: process.env.NEXT_PUBLIC_COIN_NAME || "BKC",
 };
