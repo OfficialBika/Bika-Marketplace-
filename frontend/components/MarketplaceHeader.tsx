e54b8bc4228cd5fbf9c745f626ca14c1e4e71684
Official Bika <phoetaay@gmail.com>
2026-07-15 14:21:21 +0700

    Add frontend MarketplaceHeader from Bika Marketplace merge

---
+++ frontend/components/MarketplaceHeader.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function MarketplaceHeader(){
+ return (
+  <header className="marketplace-header">
+   <h1>Bika Marketplace</h1>
+   <p>Trade • Auction • Collect</p>
+  </header>
+ );
+}
