4b43f393ae98289b3c175a334b40f7fa01a9383e
Official Bika <phoetaay@gmail.com>
2026-07-15 13:55:42 +0700

    Add backend models init from Bika Marketplace v28

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,11 @@
+from .character import Character
+from .user import User
+from .wallet import Wallet
+from .trade import Trade
+
+__all__ = [
+    'Character',
+    'User',
+    'Wallet',
+    'Trade'
+]
