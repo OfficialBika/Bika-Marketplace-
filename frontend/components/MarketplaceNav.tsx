4853f69e1d6adfceddfc8dd1b767812a0776cecb
Official Bika <phoetaay@gmail.com>
2026-07-15 14:09:24 +0700

    Add frontend MarketplaceNav from Bika Marketplace v28 ZIP

---
+++ frontend/components/MarketplaceNav.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function MarketplaceNav(){
+ return (
+  <nav className="marketplace-nav">
+   <a href="/marketplace">Marketplace</a>
+   <a href="/auction">Auction</a>
+   <a href="/wallet">Wallet</a>
+   <a href="/trade">Trade</a>
+  </nav>
+ );
+}
