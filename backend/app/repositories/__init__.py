a2f5118b634070e71342c1be08d81a0ab1d904d6
Official Bika <phoetaay@gmail.com>
2026-07-15 13:54:53 +0700

    Add backend repositories init from Bika Marketplace v28

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,11 @@
+from .character_repository import character_repository
+from .wallet_repository import wallet_repository
+from .trade_repository import trade_repository
+from .user_repository import user_repository
+
+__all__ = [
+    'character_repository',
+    'wallet_repository',
+    'trade_repository',
+    'user_repository'
+]
