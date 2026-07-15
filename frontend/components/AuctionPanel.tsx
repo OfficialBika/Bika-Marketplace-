7b7111146402140f29c41e6a086767a5975e61e2
Official Bika <phoetaay@gmail.com>
2026-07-15 14:08:23 +0700

    Add frontend AuctionPanel from Bika Marketplace v28 ZIP

---
+++ frontend/components/AuctionPanel.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function AuctionPanel(){
+ return (
+  <section className="auction-panel">
+   <h2>Auction Center</h2>
+   <p>Bid and manage marketplace auctions.</p>
+   <button>START BID</button>
+  </section>
+ );
+}
