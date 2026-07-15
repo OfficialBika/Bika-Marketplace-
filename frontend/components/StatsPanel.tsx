fed73a9340aa56ac7ced6418974f25b34e8021ef
Official Bika <phoetaay@gmail.com>
2026-07-15 14:28:10 +0700

    Add frontend StatsPanel from Bika Marketplace merge

---
+++ frontend/components/StatsPanel.tsx
@@ -0,0 +1,7 @@
+export default function StatsPanel(){
+ return <section className="stats-panel grid gap-3">
+  <div>Collections: 0</div>
+  <div>Trades: 0</div>
+  <div>Volume: 0 BKC</div>
+ </section>;
+}
