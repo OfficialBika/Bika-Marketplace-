0e0a9f00722ffe561c2e02a857aa26ab521993c8
Official Bika <phoetaay@gmail.com>
2026-07-15 13:56:28 +0700

    Update backend settings config from Bika Marketplace v28

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Settings:
     APP_NAME = os.getenv('APP_NAME', 'Bika Marketplace')
     DB_NAME = os.getenv('DB_NAME', 'bika_bot')
@@ -7,5 +8,8 @@ class Settings:
     NFT_NAME = os.getenv('NFT_NAME', 'BIKA')
     MONGO_URL = os.getenv('MONGO_URL', 'mongodb://localhost:27017')
     ENV = os.getenv('ENV', 'development')
+    JWT_SECRET = os.getenv('JWT_SECRET', 'change_me')
+    CORS_ORIGINS = os.getenv('CORS_ORIGINS', '*')
+
 
 settings = Settings()
